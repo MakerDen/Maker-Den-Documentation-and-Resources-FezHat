194114495840e5a2de4699e4931a51697c9806bb
Dave Glover <dglover@microsoft.com>
2016-06-01 18:23:43 +1000

    bootstrapper

--- Resources/Setup/CloneMakerDen.bat
+++ Resources/Setup/CloneMakerDen.bat
@@ -22,5 +22,5 @@ REM Copy Maker Den specific remote device config name
 REM copy %SetupResources%VisualStudio\launcher.csproj.user.RPiName %Solution%Launcher\launcher.csproj.user /y
 
 
-start "Start Visual Studio 2015" /d "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\" /b  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"  /resetsettings %SetupResources%\VisualStudio\MakerDenSettings.vssettings %Solution%MakerDenFEZHAT.sln
+start "Start Visual Studio 2015" /d "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\" /b  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"  /resetsettings %SetupResources%\VisualStudio\MakerDenSettings.vssettings %Solution%MakerDenFEZHAT\MakerDenFEZHAT.sln
 
