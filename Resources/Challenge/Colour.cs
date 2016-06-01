790dcfb5653829b9a3d4be826623d1babb5b22ba
Dave Glover <dglover@microsoft.com>
2016-06-01 19:04:56 +1000

    Added Colour Challenge

---
+++ Resources/Challenge/Colour.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace VividChallenge
+{
+    public class Colour
+    {
+        public int MsgId { get; set; }
+        public int[] LightId { get; set; }
+        public byte Red { get; set; } = 0;
+        public byte Green { get; set; } = 0;
+        public byte Blue { get; set; } = 0;
+        public byte White { get; set; } = 0;
+        public byte Ctrl { get; set; } = 0;
+
+        public byte[] ToJson() {
+            MsgId++;
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(this));
+        }
+    }
+}
